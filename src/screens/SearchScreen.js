a8ec1142749aebc338309c2d6fe2ac9c174caa38
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-08-17 08:16:27 +0200

    Connected search api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
-
+import { StyleSheet, Text, View } from 'react-native';
 const SearchScreen = () => {
   const [term, setTerm] = React.useState('');
+  const [results, setResults] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState('');
 
+  const searchRestaurants = async () => {
+    try {
+      const response = await yelp.get(`/search`, {
+        params: { term, limit: 50, location: 'Lamezia Terme' },
+      });
+      const restaurants = response.data.businesses;
+      setResults(restaurants);
+    } catch (err) {
+      setErrorMessage('Something went wrong, try later.');
+    }
+  };
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(term) => setTerm(term)}
-        onTermSubmit={() => console.log('submitted')}
+        onTermChange={setTerm}
+        onTermSubmit={searchRestaurants}
       />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
     </View>
   );
 };
