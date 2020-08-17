fab85b48a78a8e33f5a0a96a429ea3e769300ed5
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-08-17 07:38:09 +0200

    Added navigation stack

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import SearchScreen from './src/screens/SearchScreen';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const navigationMap = {
+  Search: SearchScreen,
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigationConfig = {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: { title: 'Cerca un ristorante' },
+};
+const navigator = createStackNavigator(navigationMap, navigationConfig);
+
+export default createAppContainer(navigator);
